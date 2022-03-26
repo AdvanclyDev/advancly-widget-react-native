a2e957ba7ca6eb321dac0fd094622b8608ddf34d
Gbenga Olufeyimi <advanclydev@gmail.com>
2022-03-26 17:44:41 +0100

    Improved default export

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-import { NativeModules } from 'react-native';
 import AdvanclyWidget from './AdvanclyWidget';
 import * as advanclyProps from './types';
 
 export { AdvanclyWidget, advanclyProps };
 
-export default NativeModules.AdvanclyWidget;
+export default AdvanclyWidget;
